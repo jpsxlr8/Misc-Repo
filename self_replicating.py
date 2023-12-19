6d28381ee39af79587109e138b644e33319c7191
jpsxlr8 <jai.20185007@mnnit.ac.in>
2023-12-19 20:15:53 +0530

    Update self_replicating.py

--- self_replicating.py
+++ self_replicating.py
@@ -2,4 +2,4 @@
 s='s=%r;print(s%%s)';print(s%s)
 
 # interactive intron
-t='';s='t=input()or t;print(f"t={repr(t)};s={repr(s)};exec(s)#{t}")';exec(s)# 
+t='';s='t=input()or t;print(f"t={repr(t)};s={repr(s)};exec(s)#{t}")';exec(s)
